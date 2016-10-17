c2b98ea179ad69829f1416502012707b1fe2a13f
Nutmos <nuttapong_mos@hotmail.com>
2016-10-17 21:43:00 +0700

    register form completed

--- register/models.py
+++ register/models.py
@@ -2,13 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 from mongoengine import *
+from mongoengine.django.auth import User
 
 # Create your models here.
 
-class Users(Document):
-    firstName = StringField(max_length=30)
-    lastName = StringField(max_length=30)
-    middleName = StringField(max_length=30)
-    gender = StringField(max_length=2)
-    dateOfBirth = StringField(max_length=20)
-    password = StringField(max_length=128)
+class User(User):
+    email = StringField(max_length=100)
+
+#class Users(Document):
+#    firstName = StringField(max_length=30)
+#    lastName = StringField(max_length=30)
+#    middleName = StringField(max_length=30)
+#    gender = StringField(max_length=2)
+#    dateOfBirth = StringField(max_length=20)
+#    password = StringField(max_length=128)
--- register/urls.py
+++ register/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^select/', views.index, name='select'),
+    url(r'^complete-register/', views.complete_register, name='complete_register'),
 ]
