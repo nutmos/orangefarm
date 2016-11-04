2cd2191f0f88f73f47232aa0b7cf319bda4bcb02
Nutmos <nuttapong_mos@hotmail.com>
2016-11-04 11:57:43 +0700

    change

--- company_profile/views.py
+++ company_profile/views.py
@@ -9,7 +9,6 @@ def index(request):
     if request.method == 'GET':
         com_id = request.GET.get('company_id', '')
         com1 = Company.objects.get(id=com_id)
-        print com_id
         template = loader.get_template('company_profile/index.html')
         return HttpResponse(template.render({
             'name': com1.name,
@@ -17,3 +16,16 @@ def index(request):
             'location': com1.location
             }, request))
     return HttpResponse('The page does not complete')
+
+def edit(request):
+    if request.method == 'GET':
+        com_id = request.GET.get('company_id', '')
+        com1 = Company.objects.get(id=com_id)
+        template = loader.get_template('comapny_profile/edit.html')
+        pass_data = {
+            'name': com1.name,
+            'description': com1.description,
+            'location': com1.location
+        }
+        return HttpResponse(template.render(pass_data, request))
+    return HttpResponse('The page is not complete')
