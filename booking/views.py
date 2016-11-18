cfd6b4b3524676dc08145b22a1b7f61666551b1d
Tunchanok <tunchanok@Tunchanoks-MacBook-Pro.local>
2016-11-18 13:01:07 +0700

    test booking

--- booking/views.py
+++ booking/views.py
@@ -3,13 +3,20 @@ from django.http import HttpResponse, HttpResponseRedirect
 from mongoengine import *
 from models import *
 from django.template import loader
-
+from trip.models import *
 
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('booking/index.html')
-    return HttpResponse(template.render({'foo': 'bar'}, request))
+   template = loader.get_template('booking/index.html')
+   try:
+   	trip_id = request.GET.get('trip_id', '')
+   	trip1 = Trip.objects.get(id=trip_id)
+   	return HttpResponse(template.render({'name': trip1.name, 'id': str(trip1.id)}, request))
+   except:
+    return HttpResponse(' id is not correct')
+   return HttpResponse('This page is not complete')
+    
 
 def info(request):
     template = loader.get_template('booking/info.html')
