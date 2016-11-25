47ac2e56ec86cfac70ec0c50e25252ccf08a44b1
Palastore <chawanakornkus@gmail.com>
2016-11-25 20:21:18 +0700

    fix bug booking(not test)

--- booking/models.py
+++ booking/models.py
@@ -8,18 +8,6 @@ from datetime import datetime
 
 # Create your models here.
 
-class Booking(Document):
-	trip = ReferenceField('Trip', reverse_delete_rule=CASCADE)
-	user = ReferenceField('User', reverse_delete_rule=CASCADE)
-	company_name = StringField()
-	book_date = DateTimeField(default=datetime.now())
-	people = IntField()
-	adult = IntField()
-	children = IntField()
-	member = ListField(ReferenceField('Tourist'))
-	total_price = IntField()
-	status = BooleanField()
-		
 class Tourist(Document):
 	booking = ReferenceField('Booking', reverse_delete_rule=CASCADE)
 	title = StringField()
@@ -32,3 +20,15 @@ class Tourist(Document):
 	passportno = StringField()
 	mobile = StringField()
 	email = StringField()
+
+class Booking(Document):
+	trip = ReferenceField('Trip', reverse_delete_rule=CASCADE)
+	user = ReferenceField('User', reverse_delete_rule=CASCADE)
+	company_name = StringField()
+	book_date = DateTimeField(default=datetime.now())
+	people = IntField()
+	adult = IntField()
+	children = IntField()
+	member = ListField(ReferenceField('Tourist'))
+	total_price = IntField()
+	status = BooleanField()
